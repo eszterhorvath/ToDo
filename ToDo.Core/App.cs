1f1aa3952bf44860a95fbf9af37ba062d5a2c5a0
Eszter <eszter.horvath@opti-q.com>
2020-10-22 10:24:18 +0200

    Added App.cs to start the application

---
+++ ToDo.Core/App.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MvvmCross;
+using MvvmCross.ViewModels;
+using ToDo.Core.Services;
+using ToDo.Core.ViewModels;
+
+namespace ToDo.Core
+{
+    public class App : MvxApplication
+    {
+        public override void Initialize()
+        {
+            Mvx.IoCProvider.RegisterType<IToDoService, ToDoService>();
+
+            RegisterAppStart<ToDoViewModel>();
+        }
+    }
+}

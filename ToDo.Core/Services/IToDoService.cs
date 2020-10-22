1a86802aeebb139648bc66f4d7513de0a9b5dd74
Eszter <eszter.horvath@opti-q.com>
2020-10-22 10:22:59 +0200

    Added ToDoService to the Core project

---
+++ ToDo.Core/Services/IToDoService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDo.Core.Services
+{
+    public interface IToDoService
+    {
+        void AddEntry(string title, string description);
+        void RemoveEntry(string title);
+        List<Models.ToDo> LoadEntries();
+    }
+}
---
+++ ToDo.Core/Services/ToDoService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDo.Core.Services
+{
+    public class ToDoService : IToDoService
+    {
+        public void AddEntry(string title, string description)
+        {
+            // save the entry to the db here
+        }
+
+        public void RemoveEntry(string title)
+        {
+            // remove the entry from the db here
+        }
+
+        public List<Models.ToDo> LoadEntries()
+        {
+            return new List<Models.ToDo>();
+        }
+    }
+}

03db2ea5fd7d193a8d27e561ace2ed1dc61bb229
Eszter <eszter.horvath@opti-q.com>
2020-10-22 10:23:32 +0200

    Added ToDo model class

---
+++ ToDo.Core/Models/ToDo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace ToDo.Core.Models
+{
+    public class ToDo
+    {
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+    }
+}

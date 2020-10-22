c428a4d2b0b17acd22d8909f88068b3dc3ad3068
Eszter <eszter.horvath@opti-q.com>
2020-10-22 17:11:06 +0200

    Added AddViewModel to add todos to the list

---
+++ ToDo.Core/ViewModels/AddViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using MvvmCross.ViewModels;
+using ToDo.Core.Models;
+using ToDo.Core.Services;
+
+namespace ToDo.Core.ViewModels
+{
+    public class AddViewModel : MvxViewModel
+    {
+        private readonly IToDoService _todoService;
+
+        public Models.ToDo ToDoItem { get; set; }
+
+        public AddViewModel(IToDoService todoService)
+        {
+            _todoService = todoService;
+
+            ToDoItem = new Models.ToDo();
+        }
+
+        public async void AddTodo()
+        {
+            if (ToDoItem.Title != null)
+            {
+                await _todoService.SaveTodoAsync(ToDoItem);
+            }
+        }
+    }
+}

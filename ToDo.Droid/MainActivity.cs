4a3241a4d619b742dd868e56fc8af6d28a622039
Eszter <eszter.horvath@opti-q.com>
2020-10-22 17:11:26 +0200

    Added Android project

---
+++ ToDo.Droid/MainActivity.cs
@@ -0,0 +1,23 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using MvvmCross.Forms.Platforms.Android.Core;
+using MvvmCross.Forms.Platforms.Android.Views;
+using ToDo.Core;
+using ToDo.UI;
+
+namespace ToDo.Droid
+{
+    [Activity(
+        Label = "ToDo.Droid",
+        Theme = "@style/MyTheme",
+        MainLauncher = true,
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation,
+        LaunchMode = LaunchMode.SingleTask)]
+    public class MainActivity : MvxFormsAppCompatActivity<MvxFormsAndroidSetup<App, FormsApp>, App, FormsApp>
+    {
+    }
+}

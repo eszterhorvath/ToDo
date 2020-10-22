6023214893a05a3a4ba97541b88e88072a12c3fe
Eszter <eszter.horvath@opti-q.com>
2020-10-22 17:09:53 +0200

    Added ToDo.UI project which contains the shared UI codes

---
+++ ToDo.UI/Views/AddView.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MvvmCross.Core.Parse.StringDictionary;
+using MvvmCross.Forms.Views;
+using ToDo.Core.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ToDo.UI.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AddView : MvxContentPage<AddViewModel>
+    {
+        public AddView()
+        {
+            InitializeComponent();
+        }
+
+        async void OnButtonClicked(object sender, EventArgs e)
+        {
+            ViewModel.AddTodo();
+
+            await Navigation.PopModalAsync();
+        }
+    }
+}
---
+++ ToDo.UI/Views/ToDoView.xaml.cs
@@ -0,0 +1,48 @@
+﻿using MvvmCross.Forms.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MvvmCross.Core.Parse.StringDictionary;
+using ToDo.Core.ViewModels;
+using ToDo.Core;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ToDo.UI.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ToDoView : MvxContentPage<ToDoViewModel>
+    {
+        public ToDoView()
+        {
+            InitializeComponent();
+        }
+
+        async void OnButtonClicked(object sender, EventArgs e)
+        {
+            var addPage = new AddView();
+
+            await Navigation.PushModalAsync(addPage);
+        }
+
+        async void OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem != null)
+            {
+                bool sure = await DisplayAlert("Delete item", "Are you sure you want to delete this item?", "Yes",
+                    "No");
+
+                if (sure)
+                {
+                    ViewModel.RemoveTodo((Core.Models.ToDo) e.SelectedItem);
+                }
+                else
+                {
+                    ((ListView) sender).SelectedItem = null;
+                }
+            }
+        }
+    }
+}

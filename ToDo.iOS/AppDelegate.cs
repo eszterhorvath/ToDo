b873840b291e807c011e78f5251481839d5705cc
Eszter <eszter.horvath@opti-q.com>
2020-10-28 13:01:38 +0100

    Added iOS project, Issue #7

---
+++ ToDo.iOS/AppDelegate.cs
@@ -0,0 +1,21 @@
+﻿using Foundation;
+using MvvmCross.Forms.Platforms.Ios.Core;
+using ToDo.Core;
+using ToDo.UI;
+using UIKit;
+
+namespace ToDo.iOS
+{
+    // The UIApplicationDelegate for the application. This class is responsible for launching the
+    // User Interface of the application, as well as listening (and optionally responding) to application events from iOS.
+    [Register("AppDelegate")]
+    public class AppDelegate : MvxFormsApplicationDelegate<MvxFormsIosSetup<App, FormsApp>, App, FormsApp>
+    {
+        public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
+        {
+            return base.FinishedLaunching(application, launchOptions);
+        }
+    }
+}
+
+
